cec09410628bfe941911c57c1cc8407c1df073ed
dylen chew <56499861+rgonslayer@users.noreply.github.com>
2022-06-13 00:49:59 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
+        'plugin:@next/next/recommended',
         "plugin:react/recommended"
     ],
     "parserOptions": {
