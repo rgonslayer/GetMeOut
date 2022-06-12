833f988d7f223da5a4a7529b03428d7f6a9e7c57
dylen chew <56499861+rgonslayer@users.noreply.github.com>
2022-06-13 00:52:31 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,7 @@ module.exports = {
        "react/react-in-jsx-scope": "off",
         // allow jsx syntax in js files (for next.js project)
        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }], //should add ".ts" if typescript project
+       "react/no-unescaped-entities": "off",
+       "@next/next/no-page-custom-font": "off"
     }
 }
